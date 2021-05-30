fa5790004d661421f6765845fc2104dd1f248e17
Knight <mayanks365@gmail.com>
2021-05-30 09:25:33 +0530

    commented file

--- src/public/client.js
+++ src/public/client.js
@@ -21,9 +21,11 @@
 //     }
 // }
 // import paginate from './pagination.js';
+
 function paginate(
+// Rover.prototype.paginate = (
     roverDataResponse, totalItems, currentPage = 1, pageSize = 10, maxPages = 10 ) {
-    
+    console.log("4")
     // calculate total pages
     let totalPages = Math.ceil(totalItems / pageSize);
 
@@ -63,126 +65,152 @@ function paginate(
     let endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
 
     // create an array of pages to ng-repeat in the pager control
-    let pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
+    let pages = Array.from(Array((endPage) - startPage).keys()).map(i => startPage + i);
 
     // console.log(pages)
     htmlPage(pages, totalPages)
-    console.log(roverDataResponse.length)
-    page(roverDataResponse, totalItems,pageSize )
-    // return object with all pager properties required by the view
-    return {
-        totalItems: totalItems, // 186
-        currentPage: currentPage, // 1
-        pageSize: pageSize, // 10
-        totalPages: totalPages, // 19
-        startPage: startPage, // 1
-        endPage: endPage, // 10
-        startIndex: startIndex, // 0
-        endIndex: endIndex, // 9
-        pages: pages // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    };
+    // console.log(roverDataResponse.length)
+    page(roverDataResponse, totalItems, pageSize )
+    // console.log(totalItems,pageSize)
 }
 
+let photos = {},
+globalList = [];
+
 function page( roverDataResponse, totalItems, pageSize ){
-    // console.log(roverDataResponse[0])
+    // console.log(list)
+    console.log("6")
     // console.log(roverDataResponse[1])
 
     // roverDataResponse.image.photos
-    let list = []
-    let i = 0
-    let obj = {}
+    let i = 0,
+    obj = {},
+    list = [],
+    lastIteration = totalItems;
     while (i < totalItems){
-        let subList = []; 
-        for( let j = 0; j < pageSize; j ++ ){
-            subList.push(roverDataResponse[j])
+        let subList = [], 
+        pageeSize  = Math.min( pageSize, lastIteration)
+        for( let j = 0; j < pageeSize; j ++ ){
+            subList.push(roverDataResponse.image.photos[i])
+            i++
+            lastIteration = lastIteration - 1
         }
         obj = subList;
-        list.push(obj)
+        list.push(obj);
         // console.log(obj)
-        i++;
     }
 
-    // console.log(list[0])
-
-    let limitedRoverImages = list[0];
+    // console.log(list)
+    globalList =  list;
+    photos = list[0];
+    console.log(photos)
+    console.log("photos updated")
+    // return newImage;
     // console.log(limitedRoverImages)
     // return limitedRoverImages.map( (roverImage) => this.getRoverImageSource(roverImage) ); 
-    // return limitedRoverImages.map( Rover.prototype.getRoverImageSource ); // will run 20 times
-
+    // return Rover.imageSources.map( Rover.prototype.getRoverImageSource ); // will run 10 times
 }
 
 function htmlPage(data, totalPages){    
-    
+    console.log("5")
     // pages = document.getElementsByClassName("pagination");
     pages = document.getElementById("pages");
     pages.innerHTML = 
     `
-    <ul class="pagination">
+    <ul class="pagination justify-content-center">
         ${  data.map( (data) => {
             return pages.innerHTML = 
-            `<li class="page-item"><a class="page-link" href="#"> ${data}</a></li>`
+            `<li class="roverData page-item" onclick = "currentPage( ${data} )"><a class="page-link" href="#"> ${data}</a></li>`
             } ).join("") }
     </ul>
     `
+
 }
 
-function Rover (roverD) {
+function currentPage (rederPage)  {
+    // console.log(rederPage)
+    photos = globalList[rederPage];
+    // console.log(globalList)
+    // console.log({image:{photos}})
+    let rover = new Rover( {image:{photos} }) ;
+    // console.log(rover);
+    createDynamicUI(rover);
+    // Rover.prototype.getRoverImageSource(newImage)
+}
+
+// function newRender(){
+//     let rover = new Rover();
+//     return rover;
+// }
+
+function Rover( roverD ) {
     // console.log(roverD)
     const roverData = roverD.image.photos[0] 
     this.roverName = roverData.rover.name;
+    console.log(roverData.rover.name);
     this.launchDate = roverData.rover.launch_date;
     this.landingDate = roverData.rover.landing_date;
     this.roverStatus = roverData.rover.status;
     this.recentDate = roverData.rover.max_date;
-    this.imageSources = this.getLimitedRoverImagesSource(roverD);
-
+    // this.imageSources = this.getRoverImageSource(roverD.image.photos);
+    this.getRoverImageSource(photos)
+    this.imageSources = this.getRoverImageSource(photos);
+    console.log("photos RAN")
+    // this.imageSources = this.paginate(roverD.image.photos, roverD.image.photos.length);
 }
-// function to get first 20 (fast response time to user) image src URl from the responseData using map
-Rover.prototype.getLimitedRoverImagesSource = function(roverDataResponse) {
-    // let roverData = JSON.parse(JSON.stringify(roverDataResponse));
-    // console.log(roverData)
-    // console.log(roverDataResponse)
-    /* 
-    data was object type, does't need to parse/stringify
-    */
-    // console.log(roverDataResponse.image.photos)
-    paginate(roverDataResponse.image.photos, roverDataResponse.image.photos.length)
-    // let limitedRoverImages = roverDataResponse.image.photos.splice(1, 20);
 
-    /* 
-    */
-   let limitedRoverImages = roverDataResponse.image.photos.splice(1, 20);
-   console.log(limitedRoverImages)
-   // console.log(this)
-   // return limitedRoverImages.map( (roverImage) => this.getRoverImageSource(roverImage) ); 
-   return limitedRoverImages.map( this.getRoverImageSource ); // will run 20 times
+    // function to get first 20 (fast response time to user) image src URl from the responseData using map
+//     Rover.prototype.getLimitedRoverImagesSource = function(roverDataResponse) {
+//     // let roverData = JSON.parse(JSON.stringify(roverDataResponse));
+//     // console.log(roverData)
+//     // console.log(roverDataResponse)
+//     /* 
+//     data was object type, does't need to parse/stringify
+//     */
+//     // console.log(roverDataResponse.image.photos)
+//     // paginate(roverDataResponse.image.photos, roverDataResponse.image.photos.length);
+//     /* 
+//     */
+//     let limitedRoverImages = roverDataResponse.image.photos.splice(1, 10);
+//     // console.log(limitedRoverImages)
+//     // console.log(this)
+//     // return limitedRoverImages.map( (roverImage) => this.getRoverImageSource(roverImage) ); 
+//     return limitedRoverImages.map( this.getRoverImageSource ); // will run 20 times
     
-}
+// }
 
 
 // map.(Rover.getRoverImageSource)
 
 Rover.prototype.getRoverImageSource = (roverImage) => {
-    console.log("getRoverImageSource RAN")
-    console.log(roverImage)
-    return roverImage.img_src;
+    // console.log(roverImage)
+    /* {id: 290647, sol: 10, camera: {…}, img_src: "http://mars.nasa.gov/mer/gallery/all/2/r/010/2R127262701EFF0214P1004L0M1-BR.JPG", earth_date: "2004-01-14", …} */
+    return roverImage.map( (data) => {
+        if(  data !==  undefined ){
+            // console.log(data)
+            return data.img_src
+        }
+    } );
+    // return roverImage.img_src
 }
 
 // High-order function - render UI through callback function
 async function processUI(roverName, callBack) {
-    console.log(roverName)
+    console.log("1")
+    // console.log(roverName)
     let data = await getRoverData(roverName);
     // console.log(data)
     // callBack(data);
     createDynamicUI(data);
-}
+};
 
 // let button = document.getElementById("");
 // addEventListener("click", processUI);
 
 // Calling `express server`
-async function getRoverData(roverName) {
-
+async function getRoverData( roverName ) {
+    console.log("2")
+    // console.log(roverName)
     const res = await fetch(`http://localhost:3000/nasaAPI`, {
         headers: {
             'roverName': roverName,
@@ -190,16 +218,21 @@ async function getRoverData(roverName) {
     });
     let data = await res.json();
     // console.log(data)
-    if (!data.image.hasOwnProperty("errors")) {
-        return processRoverData(data);
+    paginate(data, data.image.photos.length);
+
+    if ( !data.image.hasOwnProperty( "errors" ) ) {
+        return processRoverData(data)
     }
     return data;
-}
+};
 
 // A pure function to process rover data and parse it to `Rover` model
 function processRoverData(responseData) {
+    console.log("3")
     // let roverData = JSON.parse(JSON.stringify(responseData));
-    // console.log(responseData)
+    // let photos = responseData.image.photos.splice(1, 10);
+    // console.log(responseData);
+    // console.log({image:{photos}})
     let rover = new Rover(responseData);
     return rover;
 }
@@ -224,7 +257,7 @@ function createDynamicUI(roverData) {
         status.innerHTML = roverData.roverStatus;
 
         // let recentDate = document.getElementById("recentDate");
-        // if( roverData.recentDate !== undefined){
+        // if( roverData.recentDate !== undefined ){
         //     recentDate.innerHTML = roverData.recentDate
         // }
 
@@ -232,13 +265,16 @@ function createDynamicUI(roverData) {
         imageGallery.innerHTML = "";
 
         // let imgList = {}
-        console.log(roverData)
         roverData.imageSources.map( (roverImage) => {
-            imageEle = document.createElement("img");
+            // console.log(roverImage)
+            if( roverImage !== undefined ){
+                // console.log("Image rendered")
+                imageEle = document.createElement("img")
                 imageEle.setAttribute("src", roverImage)
-                imageEle.setAttribute("class", "col-md-3 my-3");
+                imageEle.setAttribute("class", "col-md-5 my-4");
                 imageGallery.appendChild(imageEle);
-        })
+            }
+        });
         // if( imgList.length > 0 ){
         //     console.log(imgList)
         //     imageGallery.appendChild(imgList);
@@ -251,7 +287,7 @@ function createDynamicUI(roverData) {
 
         //     imageGallery.appendChild(imageEle);
         // }
-
+        // document.getElementsByClassName("container").style.visibility = 'visible';
         document.getElementById("roverMetaInfo").style.visibility = 'visible';
         document.getElementById("roverImages").style.visibility = 'visible';
         document.getElementById("errorBox").style.visibility = 'collapse';
@@ -270,4 +306,4 @@ function handleError() {
 
     let imageGallery = document.getElementById("roverImages");
     imageGallery.style.visibility = 'collapse';
-}
+};
